8c2b5dfaac804469e67517c8165d73e7bf315adb
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-10-08 22:57:47 -0500

    preds

--- feorm.py
+++ feorm.py
@@ -16,10 +16,8 @@ import argparse
 relabel = lambda x: 0 if x=='female'  else 1
 unlabel = lambda y: 'female' if y==0  else 'male' 
 
-def get_data_frame(csv_file,istrain=True):
+def get_data_frame(csv_file):
 	df = pd.read_csv(csv_file)
-	# for now let this be as you are on dev set 
-	#if istrain:
 	labels = np.asarray([ relabel(row[-1]) for row in df.values])
 	df = df.drop(labels=df.columns[-1],axis=1)
 	return df,labels
@@ -31,26 +29,34 @@ def get_fnames(unpruned_arff):
 		for line in af:
 			pieces = line.split()
 			if data_flag:
-				fnames.append(pieces[-1].replace('_','.'))
+				temp = pieces[-1].split('_')
+				fnames.append(temp[-2]+'.'+temp[-1])
 
 			if pieces[0] == '@data':
 				data_flag = True
 	return fnames
 
+def write_submission(outfile,preds,fnames):
+	with open(outfile,'w') as out:
+		for fname,pred in zip(fnames,preds):
+			out.write('{0},{1}'.format(fname,unlabel(pred)))
+			out.write('\n')
+
+
 
 if __name__ == '__main__':
 
 	parser = argparse.ArgumentParser(description='python feorm.py --train <train-csv> --test <test-csv> --fnames <unpruned-test-arff file>')
-	parser.add_argument('-tr','--train',help='path to arff file',required=True)
-	parser.add_argument('-te','--test',help='path to arff file',required=True)
-	parser.add_argument('-fn','--fnames',help='path to arff file',required=True)
+	parser.add_argument('-tr','--train',help='path to train-csv',required=True)
+	parser.add_argument('-te','--test',help='path to test-csv',required=True)
+	parser.add_argument('-fn','--fnames',help='path to unpruned test arff file',required=True)
 	args= vars(parser.parse_args())
 
 	trdf , labels = get_data_frame(args['train'])
-	#tedf , truth = get_data_frame(args['test'])
-	#fnames = get_fnames(args['fnames'])
+	tedf , unecessary_labels = get_data_frame(args['test'])
+	fnames = get_fnames(args['fnames'])
 
-	"""#### for pyramidal
+	"""# for pyramidal
 	clf = RFC(n_estimators=50,
 	 criterion='entropy', 
 	 max_depth=None, 
@@ -58,13 +64,13 @@ if __name__ == '__main__':
 	  max_features=10, 
 	  max_leaf_nodes=None,
 	  class_weight = 'balanced'
-	)
-	#########"""
-	clf1 = svm.SVC(C=1,kernel='poly',gamma = 'auto')
-	clf2 = svm.SVC(C=1,kernel='rbf',gamma = 'auto')
+	)"""
+	# for w2v
+	clf2 = svm.SVC(C=25,kernel='rbf',gamma = 'auto')
+	# for w2v 
 	clf3 = svm.SVC(C=1,kernel='linear',gamma = 'auto')
-	svms = (clf1,clf2,clf3)
 	
+	"""svms = (clf2,clf3)
 	scoring = ['f1_macro','accuracy','precision_macro','recall_macro']
 	for clf in svms:
 		scores = cross_validate(clf,trdf,labels,scoring = scoring,cv=10,return_train_score=False)
@@ -73,22 +79,21 @@ if __name__ == '__main__':
 			for v in scores[s]:
 				print v
 			print '###############################'
-		print '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
-		
+		print '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"""
 
 	#clf.fit(trdf,labels)
-	"""clf1.fit(trdf,labels)
 	clf2.fit(trdf,labels)
-	clf3.fit(trdf,labels)"""
+	clf3.fit(trdf,labels)
 
 	#z = clf.predict(tedf)
-	"""z1 = clf1.predict(tedf)
-	z2 = clf2.predict(tedf)
-	z3 = clf3.predict(tedf)"""
+	zrbf = clf2.predict(tedf)
+	zlinear = clf3.predict(tedf)
+
+	write_submission('./w2v-svm-rbf.txt',zrbf,fnames)
+	write_submission('./w2v-svm-linear.txt',zlinear,fnames)
 	
 	#print 'RF : '
 	#print '{0}'.format(classification_report(y_true=truth,y_pred=z,target_names=['female','male']))
-	#print 'SVC linear: '
-	#print '{0}'.format(classification_report(y_true=truth,y_pred=z3,target_names=['female','male']))
+
 
 
